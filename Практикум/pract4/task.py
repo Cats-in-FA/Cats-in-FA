df2ae385ad2bf79746f6365349272757f079a71a
Georgiy Demo <demenchuk.george@protonmail.com>
2020-11-20 02:52:42 +0300

    Добавил базовый каркас tkinter
    в main пример логики формирования button с 93 до 101 строки

--- Практикум/pract4/task.py
+++ Практикум/pract4/task.py
@@ -1,18 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
 import random
+#Разрешение окна
+WIDTH = 750
+HEIGHT = 750
 
 class Cell:
     """Класс клетки на поле"""
     
-    def __init__(self, x, y, isbomb=False, value=0) -> None:
+    def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-        self.isbomb = isbomb
-        self.value = value
+
+        #С этими полями работать через гетеры/сеттеры
+        self.isbomb = None
+        self.value = None
 
     #TODO при добавлении tkinter
     def click(self):
         """Обработка нажатия"""
         pass
+
 
 class Field:
     """Класс игрового поля"""
@@ -34,32 +42,39 @@ class Field:
             matrix.append(buf_matrix)
         self.matrix = matrix
 
-    #TODO
     def filler(self):
-        """Заполнялка матрицы данными с бомбами"""
-        i = 0
+        """Заполнение матрицы данными"""
+        
         matrix = self.matrix
-        while i <= (self.n + self.m): #Рандомная пропорция на количество бомб на поле
-            a = random.randint(0, self.n-1)
-            b = random.randint(0, self.m-1)
-            if not matrix[a][b].isbomb:
-                matrix[a][b].isbomb = True
-                i += 1 #счетчик без for, не бей меня :D
-        for i in range(self.n): #Проставляем числа
+
+        #Логика расстановки бомб на игровом поле
+        bomb_counter = 0
+        while bomb_counter < (self.n + self.m):
+            x_coord, y_coord = random.randint(0, self.n-1), random.randint(0, self.m-1)
+            #Если выбранная клетка не бомба, то она станет бомбой
+            if not matrix[x_coord][y_coord].isbomb:
+                matrix[x_coord][y_coord].isbomb = True
+                bomb_counter += 1
+
+        #Логика выставления чисел
+        for i in range(self.n):
             for j in range(self.m):
-                if not matrix[i][j].isbomb: #Считаем бомбы
-                    count = 0
+                # Если это не бомба - присваиваем ей значение
+                if not matrix[i][j].isbomb:
+                    buf_value = 0
                     for a in range(3):
                         for b in range(3):
-                            if (0<=(i-1+a)<self.n) and (0<=(j-1+b)<self.m) and (matrix[i-1+a][j-1+b].isbomb): count+=1 #Если клетка внутри поля и это бомба, то считаем
-                    matrix[i][j].value = count
+                            if (0<=(i-1+a)<self.n) and (0<=(j-1+b)<self.m) and (matrix[i-1+a][j-1+b].isbomb):
+                                buf_value+=1 #Если клетка внутри поля и это бомба, то считаем
+                    matrix[i][j].value = buf_value
+        
         self.matrix = matrix
         
     def __str__(self):
         """Вывод матрицы на экран"""
         matrix = self.matrix
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
+        for i in range(self.n):
+            for j in range(self.m):
                 if matrix[i][j].isbomb:
                     print("*", end=' ')
                 else:
@@ -68,8 +83,26 @@ class Field:
         return ""
 
 def main():
-    Field1 = Field(10,10)
-    print(Field1)
+    
+    #Инициализация канваса
+    root = tk.Tk()
+    c = tk.Canvas(root, width=WIDTH, heigh=HEIGHT)
+    root.title("Сапёр")
+
+    # Ето рабочий пример-демо
+    buttons_matrix = []
+    for c in range(10):
+        row = []
+        for r in range(10):
+            button = tk.Button(root, text="{}:{}".format(c,r))
+            button.grid(row=r, column=c, sticky=tk.NW+tk.NE+tk.SW+tk.SE+tk.W+tk.E+tk.N+tk.S)
+            row.append(button)
+        buttons_matrix.append(row)
+    root.mainloop()
+    
+    #Ето вызов экземпляра Поля сапёра
+    #Field1 = Field(10,10)
+    #print(Field1)
 
 if __name__ == "__main__":
     main()
