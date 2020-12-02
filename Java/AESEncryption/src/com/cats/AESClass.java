38d9bde1473e78b128b45ebfd165f3a99c9485dd
Georgiy Demo <demenchuk.george@protonmail.com>
2020-12-02 19:08:15 +0300

    Check

--- Java/AESEncryption/src/com/cats/AESClass.java
+++ Java/AESEncryption/src/com/cats/AESClass.java
@@ -6,4 +6,17 @@ package com.cats;
  */
 
 public class AESClass {
+
+    private int nb = 4; // number of coloumn of State (for AES = 4)
+    private int nr = 10;  // number of rounds ib ciper cycle (if nb = 4 nr = 10)
+    private int nk = 4;  // the key length (in 32-bit words)
+    public AESClass(){
+
+        //# This dict will be used in SubBytes().
+        //Словарь для SubBytes();
+    }
+
+    //private
+
+
 }
