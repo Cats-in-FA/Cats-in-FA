8dbbed02cf67856c419f5bbaf03697087f68471c
Georgiy Demo <demenchuk.george@protonmail.com>
2020-12-04 21:32:11 +0300

    backup

--- Java/AESEncryption/src/com/cats/Main.java
+++ Java/AESEncryption/src/com/cats/Main.java
@@ -1,28 +1,25 @@
 package com.cats;
 
 import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     public static void main(String[] args) {
         //TODO Тут сделать файлы
+
         String InputString = "MEOW";
+        String KeyString = "3fed2c8817f0903a3fe007de83bac6dc";
         byte[] inputText = InputString.getBytes();
+        byte[] key = KeyString.getBytes();
 
-        for (int i = 0; i < inputText.length; i++) {
-            System.out.println(inputText[i]);
-        }
+        AESClass obj = new AESClass(key);
 
-        byte[] iv = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        byte[] key;
+        String CatResultEncrypt = new String(obj.ECB_decrypt(obj.ECB_encrypt(inputText)), StandardCharsets.UTF_8);
+        System.out.println("Шифровка: " + CatResultEncrypt);
 
-        // 128
-        System.out.println("128 bit");
-        //key = new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        //AESClass obj = new AESClass(key);
-        //System.out.println("ECB result --> " + new String(obj.ECB_decrypt(obj.ECB_encrypt(inputText))));
-        //obj = new AESClass(key, iv);
-        //System.out.println("CBC result --> " + new String(obj.CBC_decrypt(obj.CBC_encrypt(inputText))));
+        //String CatResultDecrypt = new String(obj.ECB_decrypt(CatResultEncrypt.getBytes()), StandardCharsets.UTF_8);
+        //System.out.println("Расшифровка: "+CatResultDecrypt);
 
     }
 }
