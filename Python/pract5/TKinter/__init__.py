1e8823600f43da8c28be9658c449d9c8dd380375
Georgiy Demo <demenchuk.george@protonmail.com>
2020-12-15 13:03:13 +0300

    Выкинули звук.и pygame

--- Python/pract5/TKinter/__init__.py
+++ Python/pract5/TKinter/__init__.py
@@ -7,5 +7,4 @@ from .image import get_height
 from .image import get_width
 from .image import load_image
 from .input import KEY_MAP
-from .sound import load_sound
 from .timers import create_timer
