44696988d0ddcba5e5f5d292f2d736d40b7e31f3
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-11-21 20:13:33 +0300

    It is working
    Co-Authored-By: KOSHKA <69214647+Cat-in-box@users.noreply.github.com>

--- Python/Information Security/gui.py
+++ Python/Information Security/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from wrapper import decrypt_logic, encrypt_logic, check_file
 
 def clear():
     for i, field in enumerate(fields):
@@ -6,20 +7,43 @@ def clear():
             field.delete(0, tk.END)
         else:
             field.config(text = "")
- 
+
+
+def encrypt_wrapper():
+    file_name = fields[0].get()
+    key = fields[1].get()
+    if check_file(file_name):
+        result = encrypt_logic(file_name, file_name, key)
+    else:
+        pass
+    fields[2].config(text = result)
+    
+def decrypt_wrapper():
+    file_name = fields[0].get()
+    key = fields[1].get()
+    if check_file(file_name):
+        result = decrypt_logic(file_name, file_name, key)
+    else:
+        pass
+    fields[2].config(text = result)
+    
+
+
+
+
 # Создается новое окно с заголовком "Введите домашний адрес".
 window = tk.Tk()
 window.title("AES")
- 
+
 # Создается новая рамка `frm_form` для ярлыков с текстом и
 # Однострочных полей для ввода информации об адресе.
 frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
 # Помещает рамку на окно приложения.
 frm_form.pack()
- 
+
 # Список ярлыков полей.
 labels = [
-    "Текст:",
+    "Путь:",
     "Ключ:",
     "Результат:",
 ]
@@ -39,29 +63,29 @@ for i, text in enumerate(labels):
     # текстовых полей в строку, чей индекс равен idx.
     label.grid(row=i, column=0, sticky="e")
     entry.grid(row=i, column=1)
- 
+
 # Создает новую рамку `frm_buttons` для размещения в ней
 # кнопок "Отправить" и "Очистить". Данная рамка заполняет
 # все окно в горизонтальном направлении с
 # отступами в 5 пикселей горизонтально и вертикально.
 frm_buttons = tk.Frame()
 frm_buttons.pack()
- 
+
 # Создает кнопку "Отправить" и размещает ее
 # справа от рамки `frm_buttons`.
 btn_submit = tk.Button(master=frm_buttons, text="Clear", command=clear)
 btn_submit.pack(side=tk.RIGHT, padx=10, ipadx=10)
- 
+
 # Создает кнопку "Очистить" и размещает ее
 # справа от рамки `frm_buttons`.
-btn_clear = tk.Button(master=frm_buttons, text="Encrypt")
+btn_clear = tk.Button(master=frm_buttons, text="Decrypt", command=decrypt_wrapper)
 btn_clear.pack(side=tk.RIGHT)
- 
+
 # Создает кнопку "Очистить" и размещает ее
 # справа от рамки `frm_buttons`.
-btn_clear = tk.Button(master=frm_buttons, text="Decrypt")
+btn_clear = tk.Button(master=frm_buttons, text="Encrypt", command=encrypt_wrapper)
 btn_clear.pack(side=tk.RIGHT, padx=10, ipadx=10)
- 
+
 
 # Запуск приложения.
 window.mainloop()
---
+++ Python/Information Security/wrapper.py
@@ -0,0 +1,71 @@
+
+from logic import encrypt, decrypt
+from pathlib import Path
+
+
+def check_file(path_to_file: str) -> bool:
+    p = Path(path_to_file)
+    return p.exists() == True and p.is_dir() == False
+
+def read_file_strings(file_name: str) -> bytes:
+
+    with open(file_name, 'r') as ff:
+        return ff.read()
+
+def read_file_bytes(file_name: str) -> bytes:
+
+    with open(file_name, 'rb') as ff:
+        return ff.read()
+
+def write_file_bytes(file_name: str, data: bytes) -> None:
+    with open(file_name, 'wb') as ff:
+        ff.write(data)
+
+def encrypt_logic(read_file_name: str, write_file_name: str, key: str) -> str:
+    data = read_file_bytes(read_file_name)
+
+    crypted_data = []
+    temp = []
+    for byte in data:
+        temp.append(byte)
+        if len(temp) == 16:
+            crypted_part = encrypt(temp, key)
+            crypted_data.extend(crypted_part)
+            del temp[:]
+    else:
+
+        if 0 < len(temp) < 16:
+            empty_spaces = 16 - len(temp)
+            for i in range(empty_spaces - 1):
+                temp.append(0)
+            temp.append(1)
+            crypted_part = encrypt(temp, key)
+            crypted_data.extend(crypted_part)
+
+    write_file_bytes(write_file_name, bytes(crypted_data))
+    return str(crypted_data)
+
+
+def decrypt_logic(read_file_name: str, write_file_name: str, key: str) -> str:
+
+    data = read_file_bytes(read_file_name)
+
+    decrypted_data = []
+    temp = []
+    for byte in data:
+        temp.append(byte)
+        if len(temp) == 16:
+            decrypted_part = decrypt(temp, key)
+            decrypted_data.extend(decrypted_part)
+            del temp[:]
+    else:
+        if 0 < len(temp) < 16:
+            empty_spaces = 16 - len(temp)
+            for i in range(empty_spaces - 1):
+                temp.append(0)
+            temp.append(1)
+            decrypted_part = encrypt(temp, key)
+            decrypted_data.extend(decrypted_part)
+
+    write_file_bytes(write_file_name, bytes(decrypted_data))
+    return read_file_strings(write_file_name)
